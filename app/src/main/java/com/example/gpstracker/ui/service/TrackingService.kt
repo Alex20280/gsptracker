6949ed8f00b1038f707f97d7e87423c27f6b2f8c
Alex <aleksandrbasanets2012@gmail.com>
2023-11-29 15:27:39 +0200

    Did some changes to the Service. Added ability to send gps data to the service periodically.

--- app/src/main/java/com/example/gpstracker/ui/service/TrackingService.kt
+++ app/src/main/java/com/example/gpstracker/ui/service/TrackingService.kt
@@ -7,14 +7,21 @@ import androidx.core.app.NotificationCompat
 import com.example.gpstracker.R
 import com.example.gpstracker.app.App
 import com.example.gpstracker.usecase.FirebaseDatabaseUseCase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class TrackingService : Service() {
 
     @Inject
     lateinit var locationServiceUseCase: FirebaseDatabaseUseCase
-
-
+    private val serviceScope = CoroutineScope(Dispatchers.Main)
+    private var locationJob: Job? = null
     private val NOTIFICATION_ID = 1234
     private val CHANNEL_ID = "running_channel"
 
@@ -23,20 +30,27 @@ class TrackingService : Service() {
         (application as App).appComponent.inject(this)
     }
 
-
     override fun onBind(p0: Intent?): IBinder? {
         return null
     }
 
-
     private fun startServiceTracking() {
-        locationServiceUseCase.saveLocation()
+        locationJob = serviceScope.launch {
+            while (isActive) {
+                locationServiceUseCase.saveLocation()
+                delay(com.example.gpstracker.BuildConfig.TRACKING_INTERVAL_MILLIS)
+            }
+        }
     }
 
+    private fun stopService(){
+        serviceScope.cancel()
+    }
 
     override fun onDestroy() {
         stopForeground(true)
         stopSelf()
+        serviceScope.cancel()
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
@@ -46,7 +60,10 @@ class TrackingService : Service() {
                 startServiceTracking()
             }
 
-            Actions.STOP.toString() -> stopSelf()
+            Actions.STOP.toString() -> {
+                stopService()
+                stopSelf()
+            }
         }
         return super.onStartCommand(intent, flags, startId)
     }
--- app/src/main/java/com/example/gpstracker/usecase/FirebaseDatabaseUseCase.kt
+++ app/src/main/java/com/example/gpstracker/usecase/FirebaseDatabaseUseCase.kt
@@ -1,36 +1,28 @@
 package com.example.gpstracker.usecase
 
 import android.annotation.SuppressLint
-import android.content.Context
-import android.location.Location
-import android.location.LocationManager
-import android.os.Bundle
 import android.os.Looper
 import android.util.Log
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.firebase.database.DatabaseReference
-import com.example.gpstracker.ui.track.LocationData
-import com.google.android.gms.location.LocationListener
 import com.google.android.gms.location.LocationRequest
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.ServerValue
 import com.google.firebase.ktx.Firebase
 import com.google.android.gms.location.LocationCallback;
-import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationResult
 
 class FirebaseDatabaseUseCase(
     private val fusedLocationClient: FusedLocationProviderClient,
-    private val database: DatabaseReference,
-    //private  val context: Context
+    private val database: DatabaseReference
 ) {
 
     @SuppressLint("MissingPermission")
     fun saveLocation() {
-
         val locationRequest = LocationRequest().apply {
-            interval = 10000 // 10 seconds
-            fastestInterval = 5000 // 5 seconds
-            smallestDisplacement = 2F// 100 meters
+            interval = com.example.gpstracker.BuildConfig.TRACKING_INTERVAL_MILLIS
+            fastestInterval = com.example.gpstracker.BuildConfig.TRACKING_INTERVAL_MILLIS
+            smallestDisplacement = com.example.gpstracker.BuildConfig.TRACKING_INTERVAL_METERS.toFloat()
         }
 
         val locationCallback = object : LocationCallback() {
@@ -41,32 +33,15 @@ class FirebaseDatabaseUseCase(
                 }
             }
         }
-
-        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper())
-
-        // Get initial location
-        fusedLocationClient.lastLocation
-            .addOnSuccessListener { location : Location? ->
-                if(location != null) {
-                    saveLocationToFirebase(location.latitude, location.longitude)
-                }
-            }
+        fusedLocationClient.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            Looper.myLooper()
+        )
     }
 
 
-
-/*    @SuppressLint("MissingPermission")
-    fun saveLocation() {
-        // Start location tracking
-        fusedLocationClient.lastLocation
-            .addOnSuccessListener { location: Location? ->
-                if (location != null) {
-                    saveLocationToFirebase(location.latitude, location.longitude)
-                }
-            }
-    }*/
-
-    fun saveLocationFromRoomDatabase(latitude: Double, longitude: Double, ): Boolean {
+    fun saveLocationFromRoomDatabase(latitude: Double, longitude: Double): Boolean {
         saveLocationToFirebase(latitude, longitude)
         return true
     }
@@ -93,6 +68,43 @@ class FirebaseDatabaseUseCase(
             userId.toString() + latitude.toString() + "longitude" + longitude.toString() + "timestamp" + ServerValue.TIMESTAMP
         )
     }
+
+
+    /*    @SuppressLint("MissingPermission")
+        override fun getLocationUpdates(interval: Long): Flow<Location> {
+            return callbackFlow {
+
+                val locationManager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+                val isGpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                val isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+                if(!isGpsEnabled && !isNetworkEnabled) {
+                    throw LocationClient.LocationException("GPS is disabled")
+                }
+
+                val request = LocationRequest.create()
+                    .setInterval(interval)
+                    .setFastestInterval(interval)
+
+                val locationCallback = object : LocationCallback() {
+                    override fun onLocationResult(result: LocationResult) {
+                        super.onLocationResult(result)
+                        result.locations.lastOrNull()?.let { location ->
+                            launch { send(location) }
+                        }
+                    }
+                }
+
+                fusedLocationClient.requestLocationUpdates(
+                    request,
+                    locationCallback,
+                    Looper.getMainLooper()
+                )
+
+                awaitClose {
+                    fusedLocationClient.removeLocationUpdates(locationCallback)
+                }
+            }
+        }*/
 }
 /*   @SuppressLint("MissingPermission")
     fun getCurrentLocation(callback: (LocationData?) -> Unit) {
